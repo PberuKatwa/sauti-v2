d370e354e37ecc65484abdb156a08a9e992ddcb3
Pberu <pberu8531@gmail.com>
2026-03-13 16:48:49 +0300

    Client Model setup v3:setup controller

--- src/modules/client/client.module.ts
+++ src/modules/client/client.module.ts
@@ -1,8 +1,10 @@
 import { Module } from "@nestjs/common";
 import { ClientModel } from "./client.model";
 import { PostgresModule } from "../../databases/postgres.module";
+import { ClientsController } from "./client.controller";
 
 @Module({
+  controllers:[ClientsController],
   imports:[PostgresModule],
   providers:[ClientModel]
 })
