0ad75af882b53b9609ebe8ecf87706bbbf8dcc7c
Pberu <pberu8531@gmail.com>
2026-03-13 16:17:58 +0300

    Client Model setup v1:setup model

--- src/modules/client/client.model.ts
+++ src/modules/client/client.model.ts
@@ -1,6 +1,35 @@
 import { Inject, Injectable } from "@nestjs/common";
+import { Pool } from "pg";
+import { PostgresConfig } from "../../databases/postgres.config";
+import { APP_LOGGER } from "../../logger/logger.provider";
+import type { AppLogger } from "../../logger/winston.logger";
 
 @Injectable()
 export class ClientModel{
+
+  constructor(
+    @Inject(APP_LOGGER) private readonly logger: AppLogger,
+    private readonly pgConfig: PostgresConfig,
+  ) { }
+
+  createTable() {
+    try {
+
+      const query = `
+
+        CREATE TABLE IF NOT EXISTS clients (
+          id PRIMARY KEY,
+          first_name TEXT DEFAULT 'uncomfirmed',
+          last_name TEXT DEFAULT 'uncomfirmed',
+          phone_number BIGINT NOT NULL,
+          created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        )
+
+      `
+
+    } catch (error) {
+      throw error;
+    }
+  }
 
 }
--- src/modules/client/client.module.ts
+++ src/modules/client/client.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { ClientModel } from "./client.model";
+import { PostgresModule } from "../../databases/postgres.module";
 
 @Module({
+  imports:[PostgresModule],
   providers:[ClientModel]
 })
 export class ClientModule { };
