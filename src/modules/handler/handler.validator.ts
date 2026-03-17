f878e5b3ddf2ebbf99e40d4abe4d3f2fb694458b
Pberu <pberu8531@gmail.com>
2026-03-17 17:56:07 +0300

    Handler validator v3:added zod validator

--- src/modules/handler/handler.validator.ts
+++ src/modules/handler/handler.validator.ts
@@ -1,3 +1,9 @@
+import { Inject } from "@nestjs/common";
+import { APP_LOGGER } from "../../logger/logger.provider";
+import { AppLogger } from "../../logger/winston.logger";
+
 export class HandlerValidator{
+
+  constructor( @Inject(APP_LOGGER) private readonly logger:AppLogger ) { };
 
 }
