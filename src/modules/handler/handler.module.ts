568abe95f2d2609efc5d9ce287735cc9e9630100
Pberu <pberu8531@gmail.com>
2026-03-17 17:43:43 +0300

    Handler v1:added module

--- src/modules/handler/handler.module.ts
+++ src/modules/handler/handler.module.ts
@@ -1,6 +1,9 @@
 import { Module } from "@nestjs/common";
+import { HandlerValidator } from "./handler.validator";
 
 @Module({
+
+  providers:[HandlerValidator]
 
 })
 export class HandlerModule { };
