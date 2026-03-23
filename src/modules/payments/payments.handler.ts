355744155fd63c25eb7e268e2bd09cd9aa10d14c
Pberu <pberu8531@gmail.com>
2026-03-23 17:37:36 +0300

    Payments module v3:added handler

--- src/modules/payments/payments.handler.ts
+++ src/modules/payments/payments.handler.ts
@@ -1,3 +1,14 @@
+import { Inject } from "@nestjs/common";
+import { APP_LOGGER } from "../../logger/logger.provider";
+import { AppLogger } from "../../logger/winston.logger";
+import { WhatsappService } from "../whatsapp/whatsapp.service";
+
 export class PaymentsHandler{
+
+  constructor(
+    @Inject(APP_LOGGER) private readonly logger: AppLogger,
+    private readonly whatsappService:WhatsappService
+  ) { };
+
 
 }
