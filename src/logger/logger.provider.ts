30a2a0814468874bcbe4edbc2d90f70d691353ad
Pberu <pberu8531@gmail.com>
2026-03-30 20:25:06 +0300

    App logger v4:removed provider

--- src/logger/logger.provider.ts
+++
@@ -1,8 +0,0 @@
-// import { logger } from "./winston.logger";
-
-// export const APP_LOGGER = 'APP_LOGGER';
-
-// export const loggerProvider = {
-//   provide:APP_LOGGER,
-//   useValue:logger
-// }
