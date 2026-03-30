348e68d4f4498b4dba7bd285fe109c617a66adfa
Pberu <pberu8531@gmail.com>
2026-03-30 21:27:03 +0300

    Whatsapp Config setup v2:created create method

--- src/modules/whatsapp/whatsapp.config.ts
+++ src/modules/whatsapp/whatsapp.config.ts
@@ -1,10 +1,47 @@
+import { PostgresConfig } from "../../databases/postgres.config";
 import { AppLogger } from "../../logger/winston.logger";
 
 export class WhatsappConfig{
 
   constructor(
-    private readonly logger:AppLogger
-  ){}
+    private readonly logger: AppLogger,
+    private readonly pgConfig:PostgresConfig
+  ) { }
+
+  async createTable() {
+    try {
+
+      const query = `
+        CREATE TABLE IF NOT EXISTS whatsapp_config(
+
+          id SERIAL PRIMARY KEY,
+          user_id INTEGER NOT NULL,
+          status row_status DEFAULT 'active',
+          phone_number BIGINT NOT NULL UNIQUE,
+          phone_number_id BIGINT NOT NULL UNIQUE,
+          business_account_id BIGINT NOT NULL,
+          access_token TEXT NOT NULL UNIQUE,
+          permanent_token TEXT,
+          created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+
+          FOREIGN_KEY(user_id)
+            REFERENCES users(id)
+            ON DELETE SET NULL
+
+        );
+      `
+
+      const pgPool = this.pgConfig.getPool();
+
+      await pgPool.query(query)
+
+      this.logger.info(`Successfully created whatsapp config table`)
+      return "whatsapp_config"
+
+    } catch (error) {
+      throw error;
+    }
+  }
 
 
 }
