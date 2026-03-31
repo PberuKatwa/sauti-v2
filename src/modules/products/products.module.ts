0f0416e08f6228bf04dbc7608241480e2365b773
Pberu <pberu8531@gmail.com>
2026-03-31 21:07:12 +0300

    catalogue service v2:added service

--- src/modules/products/products.module.ts
+++ src/modules/products/products.module.ts
@@ -1,10 +1,11 @@
 import { Module } from "@nestjs/common";
 import { ProductsHandler } from "./products.handler";
 import { WhatsappModule } from "../whatsapp/whatsapp.module";
+import { CatalogService } from "./catalog.service";
 
 @Module({
   imports: [WhatsappModule],
-  providers: [ProductsHandler],
-  exports: [ProductsHandler]
+  providers: [ProductsHandler, CatalogService],
+  exports: [ProductsHandler, CatalogService]
 })
 export class ProductsModule { };
