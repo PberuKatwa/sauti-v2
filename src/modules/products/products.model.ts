b598275f8cb005e31d97dc472a256d5688bade3a
Pberu <pberu8531@gmail.com>
2026-03-31 17:25:32 +0300

    Product model v1:added model

--- src/modules/products/products.model.ts
+++ src/modules/products/products.model.ts
@@ -1,6 +1,13 @@
 import { Injectable } from "@nestjs/common";
+import { AppLogger } from "../../logger/winston.logger";
+import { PostgresConfig } from "../../databases/postgres.config";
 
 @Injectable()
 export class ProductModel{
+
+  constructor(
+    private readonly logger: AppLogger,
+    private readonly pgConfig:PostgresConfig
+  ) { };
 
 }
