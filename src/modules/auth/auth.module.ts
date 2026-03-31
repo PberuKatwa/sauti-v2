104b644694e2c277f93a1e1aa37dae705861edac
Pberu <pberu8531@gmail.com>
2026-03-31 15:12:51 +0300

    auth setup v4:setup controller

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,6 +1,9 @@
 import { Module } from "@nestjs/common";
+import { UsersModule } from "../users/users.module";
 
 @Module({
-
+  imports: [UsersModule],
+  providers: [AuthModule],
+  exports:[AuthModule]
 })
 export class AuthModule { };
