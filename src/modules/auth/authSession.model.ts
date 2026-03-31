58215648fef922059022932987a42e4f0b1e469d
Pberu <pberu8531@gmail.com>
2026-03-31 15:27:17 +0300

    auth session setup v2:added the table

--- src/modules/auth/authSession.model.ts
+++ src/modules/auth/authSession.model.ts
@@ -9,4 +9,32 @@ export class AuthSessionModel{
     private readonly pgConfig:PostgresConfig
   ) { };
 
+  async createTable() {
+    try {
+      const pgPool = this.pgConfig.getPool()
+
+      const query = `
+        CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+        CREATE TABLE IF NOT EXISTS auth_sessions (
+          id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+          user_id INTEGER NOT NULL,
+          status row_status DEFAULT 'active',
+          expires_at TIMESTAMP NOT NULL,
+          created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+
+          FOREIGN KEY (user_id)
+            REFERENCES users(id)
+            ON DELETE CASCADE
+
+        );
+      `;
+
+      await pgPool.query(query);
+      this.logger.info("Auth sessions table created successfully");
+    } catch (error) {
+      throw error;
+    }
+  }
+
 }
