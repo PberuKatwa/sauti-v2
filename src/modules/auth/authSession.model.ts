02ddf0c6332f165eb5fef5841f44ffbaa242fcf2
Pberu <pberu8531@gmail.com>
2026-03-31 15:24:30 +0300

    auth session setup v1:added model

--- src/modules/auth/authSession.model.ts
+++ src/modules/auth/authSession.model.ts
@@ -1,3 +1,12 @@
+import { PostgresConfig } from "../../databases/postgres.config";
+import { AppLogger } from "../../logger/winston.logger";
+
 export class AuthSessionModel{
+
+
+  constructor(
+    private readonly logger: AppLogger,
+    private readonly pgConfig:PostgresConfig
+  ) { };
 
 }
