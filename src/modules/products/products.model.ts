e07c1e1fe806914e084ec6a61075c0658601fc46
Pberu <pberu8531@gmail.com>
2026-04-01 12:29:47 +0300

    Product Model setup v1:added create table

--- src/modules/products/products.model.ts
+++ src/modules/products/products.model.ts
@@ -10,8 +10,34 @@ export class ProductModel{
     private readonly pgConfig:PostgresConfig
   ) { };
 
+  // export interface CreateProductDto {
+  //   retailerId: string;
+  //   name: string;
+  //   description?: string;
+  //   price: number;
+  //   currency: string;
+  //   availability?: 'in stock' | 'out of stock' | 'preorder' | 'available for order' | 'discontinued' | 'pending';
+  //   brand?: string;
+  //   category?: string;
+  //   imageUrl?: string;
+  //   inventory?: number;
+  // }
+
   createTable() {
     try {
+
+      const query = `
+
+        CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+        CREATE TABLE IF NOT EXISTS products(
+
+          id SERIAL PRIMARY KEY,
+          retailer_id UUID DEFAULT uuid_generate_v4(),
+
+        );
+
+      `
 
     } catch (error) {
       throw error;
