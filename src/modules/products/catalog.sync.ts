b8fbf28175074f16184e233111e5e5001a4d750d
Pberu <pberu8531@gmail.com>
2026-04-01 18:07:12 +0300

    Catalog sync v2:added create catalog

--- src/modules/products/catalog.sync.ts
+++ src/modules/products/catalog.sync.ts
@@ -6,11 +6,23 @@ import { ConfigService } from "@nestjs/config";
 @Injectable()
 export class CatalogSync{
 
+  private readonly baseS3Url:string
+
   constructor(
     private readonly logger: AppLogger,
     private readonly catalogService: CatalogService,
     private readonly configService:ConfigService
-  ) { };
+  ) {
+    this.baseS3Url = `${configService.get<string>("s3Endpoint")}/${configService.get<string>("s3Bucket")}`
+  };
+
+  async createCatalogProduct() {
+    try {
+
+    } catch (error) {
+      throw error;
+    }
+  }
 
 
 }
