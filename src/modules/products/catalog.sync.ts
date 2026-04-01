d1528616dd40d9ea28e394e7b4ae0b90896ddfc9
Pberu <pberu8531@gmail.com>
2026-04-01 19:33:52 +0300

    Catalog sync v5:added catalog sync

--- src/modules/products/catalog.sync.ts
+++ src/modules/products/catalog.sync.ts
@@ -45,6 +45,7 @@ export class CatalogSync{
 
       const catalogProduct = await this.catalogService.createProduct(this.catalogId, catalogPayload);
 
+      this.logger.info(`Successfully created catalog product`)
       return catalogProduct;
     } catch (error) {
       throw error;
