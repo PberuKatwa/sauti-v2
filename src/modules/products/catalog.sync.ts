227315fe66f223a1344d72d20d61249a50d41087
Pberu <pberu8531@gmail.com>
2026-04-01 18:01:29 +0300

    Catalog sync v1:added class

--- src/modules/products/catalog.sync.ts
+++ src/modules/products/catalog.sync.ts
@@ -1,14 +1,16 @@
 import { Injectable } from "@nestjs/common";
 import { AppLogger } from "../../logger/winston.logger";
 import { CatalogService } from "./catalog.service";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class CatalogSync{
 
   constructor(
     private readonly logger: AppLogger,
-    private readonly catalogService:CatalogService
-  ){}
+    private readonly catalogService: CatalogService,
+    private readonly configService:ConfigService
+  ) { };
 
 
 }
