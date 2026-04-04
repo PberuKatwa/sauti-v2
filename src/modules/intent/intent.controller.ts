47965266582c31ca728950d8eee6a3e327443aac
Pberu <pberu8531@gmail.com>
2026-04-04 20:03:10 +0300

    Intent Controller v4:added extract payload function

--- src/modules/intent/intent.controller.ts
+++ src/modules/intent/intent.controller.ts
@@ -1,11 +1,13 @@
-import { Controller, Post, Req, Res } from "@nestjs/common";
+import { Controller, Post, Req, Res, UseGuards } from "@nestjs/common";
 import type { Request, Response } from "express";
 import { AppLogger } from "../../logger/winston.logger";
 import { PayloadExtractor } from "./payload.extractor";
 import { ApiResponse } from "../../types/api.types";
+import { AuthGuard } from "../auth/guards/auth.guard";
 
 
 @Controller('intent')
+@UseGuards(AuthGuard)
 export class IntentController{
 
   constructor(
@@ -20,6 +22,17 @@ export class IntentController{
   ) {
     try {
 
+      const { text } = req.body;
+
+      const nouns = await this.extractor.extractCoreNouns(text);
+
+      const response: ApiResponse = {
+        success: true,
+        message: "Successfully extracted payload",
+        data:nouns
+      }
+
+      return res.status(200).json(response)
     } catch (error) {
 
       this.logger.error(`Error in extracting payload`, error);
