d7be9e7f77b66b8e58ff8df0d942f30a929826dc
Pberu <pberu8531@gmail.com>
2026-04-04 19:52:17 +0300

    Intent Controller v3:added extract payload

--- src/modules/intent/intent.controller.ts
+++ src/modules/intent/intent.controller.ts
@@ -1,6 +1,9 @@
 import { Controller, Post, Req, Res } from "@nestjs/common";
+import type { Request, Response } from "express";
 import { AppLogger } from "../../logger/winston.logger";
 import { PayloadExtractor } from "./payload.extractor";
+import { ApiResponse } from "../../types/api.types";
+
 
 @Controller('intent')
 export class IntentController{
@@ -15,6 +18,19 @@ export class IntentController{
     @Req() req: Request,
     @Res() res:Response
   ) {
+    try {
+
+    } catch (error) {
+
+      this.logger.error(`Error in extracting payload`, error);
+
+      const response: ApiResponse = {
+        success: false,
+        message: `${error}`
+      };
+
+      return res.status(500).json(response);
+    }
 
   }
 
