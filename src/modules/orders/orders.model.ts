8e38a64d34d1e9c69984148ae71a86e0dd2160f4
Pberu <pberu8531@gmail.com>
2026-04-06 16:49:35 +0300

    Order model setup v2:removed try catch

--- src/modules/orders/orders.model.ts
+++ src/modules/orders/orders.model.ts
@@ -20,73 +20,68 @@ export class OrdersModel {
   ) { }
 
   async createTable(): Promise<string> {
-    try {
 
-      this.logger.warn(`Attempting to create orders table`);
+    this.logger.warn(`Attempting to create orders table`);
 
-      const query = `
+    const query = `
 
-        DO $$
-        BEGIN
-            IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'order_status') THEN
-            CREATE TYPE order_status AS ENUM ('pending_location', 'pending_contact', 'pending_delivery_type', 'pending_delivery', 'enroute', 'delivered');
-            END IF;
-        END
-        $$;
+      DO $$
+      BEGIN
+          IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'order_status') THEN
+          CREATE TYPE order_status AS ENUM ('pending_location', 'pending_contact', 'pending_delivery_type', 'pending_delivery', 'enroute', 'delivered');
+          END IF;
+      END
+      $$;
 
-        DO $$
-        BEGIN
-            IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'types_of_delivery') THEN
-            CREATE TYPE types_of_delivery AS ENUM ('scheduled', 'immediate');
-            END IF;
-        END
-        $$;
+      DO $$
+      BEGIN
+          IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'types_of_delivery') THEN
+          CREATE TYPE types_of_delivery AS ENUM ('scheduled', 'immediate');
+          END IF;
+      END
+      $$;
 
-        CREATE SEQUENCE order_number_seq START 100;
+      CREATE SEQUENCE order_number_seq START 100;
 
-        CREATE TABLE IF NOT EXISTS orders (
-          id SERIAL PRIMARY KEY,
-          client_id INTEGER NOT NULL,
+      CREATE TABLE IF NOT EXISTS orders (
+        id SERIAL PRIMARY KEY,
+        client_id INTEGER NOT NULL,
 
-          subtotal NUMERIC(10,2) NOT NULL,
-          tax NUMERIC(10,2) DEFAULT 0,
-          total NUMERIC(10,2) NOT NULL,
+        subtotal NUMERIC(10,2) NOT NULL,
+        tax NUMERIC(10,2) DEFAULT 0,
+        total NUMERIC(10,2) NOT NULL,
 
-          status order_status',
-          latitude DECIMAL(10, 8),
-          longitude DECIMAL(11, 8),
-          order_contact BIGINT,
-          delivery_type types_of_delivery DEFAULT 'immediate',
+        status order_status',
+        latitude DECIMAL(10, 8),
+        longitude DECIMAL(11, 8),
+        order_contact BIGINT,
+        delivery_type types_of_delivery DEFAULT 'immediate',
 
-          order_number INTEGER UNIQUE DEFAULT NEXTVAL('order_number_seq'),
-          special_intructions VARCHAR(240),
+        order_number INTEGER UNIQUE DEFAULT NEXTVAL('order_number_seq'),
+        special_intructions VARCHAR(240),
 
-          created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-          updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
 
-          FOREIGN KEY(client_id)
-            REFERENCES clients(id)
-            ON DELETE SET NULL
-        );
+        FOREIGN KEY(client_id)
+          REFERENCES clients(id)
+          ON DELETE SET NULL
+      );
 
-        DROP TRIGGER IF EXISTS update_orders_timestamp ON orders;
+      DROP TRIGGER IF EXISTS update_orders_timestamp ON orders;
 
-        CREATE TRIGGER update_orders_timestamp
-        BEFORE UPDATE ON orders
-        FOR EACH ROW
-        EXECUTE FUNCTION set_timestamp();
-      `;
+      CREATE TRIGGER update_orders_timestamp
+      BEFORE UPDATE ON orders
+      FOR EACH ROW
+      EXECUTE FUNCTION set_timestamp();
+    `;
 
-      const pool = this.pgConfig.getPool();
-      await pool.query(query);
+    const pool = this.pgConfig.getPool();
+    await pool.query(query);
 
-      this.logger.info(`Successfully created orders table`);
+    this.logger.info(`Successfully created orders table`);
 
-      return "orders";
-
-    } catch (error) {
-      throw error;
-    }
+    return "orders";
   }
 
 }
