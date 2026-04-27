67dd17d4ce0bc6a6a62405215d493d1778c3d64b
Pberu <pberu8531@gmail.com>
2026-04-27 12:18:12 +0300

    RBAC v6:added roles

---
+++ src/modules/auth/guards/roles.guard.ts
@@ -0,0 +1,38 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { ROLES_KEY } from "../roles/roles.decorator";
+import { RequestWithUser, UserRoles } from "../../../types/authSession.types";
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRoles = this.reflector.getAllAndOverride<UserRoles[]>(
+      ROLES_KEY,
+      [context.getHandler(), context.getClass()],
+    );
+
+    if (!requiredRoles) return true;
+
+    const request = context.switchToHttp().getRequest<RequestWithUser>();
+    const user = request.user;
+
+    if (!user) {
+      throw new UnauthorizedException("No user found");
+    }
+
+    const hasRole = requiredRoles.includes(user.userRole);
+
+    if (!hasRole) {
+      throw new UnauthorizedException("You are not authorized");
+    }
+
+    return true;
+  }
+}
