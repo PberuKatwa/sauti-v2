e709cc08143d368b7cf4162931c2504bef4ba765
Pberu <pberu8531@gmail.com>
2026-05-03 09:17:21 +0300

    Invoices setup v1:added invoices

---
+++ src/modules/payments/invoices.model.ts
@@ -0,0 +1,50 @@
+import { Injectable } from "@nestjs/common";
+import { AppLogger } from "../../logger/winston.logger";
+import { PostgresConfig } from "../../databases/postgres.config";
+
+
+@Injectable()
+export class InvoicesModel{
+
+  constructor(
+    private readonly logger: AppLogger,
+    private readonly pgConfig:PostgresConfig
+  ) { }
+
+  async createTable() {
+
+    this.logger.warn(`Attempting to create an invoices table`);
+
+    const query = `
+
+      DO $$
+      BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'invoice_status') THEN
+        CREATE TYPE invoice_status AS ENUM ('unpaid', 'partially_paid', 'paid', 'over_paid');
+        END IF;
+      END
+      $$;
+
+      CREATE TABLE IF NOT EXISTS invoices(
+
+        id SERIAL PRIMARY KEY,
+        order_id REFERENCES orders(id) ON DELETE CASCADE,
+        invoice_number INTEGER NOT NULL UNIQUE DEFAULT NEXTVAL('invoice_number_seq'),
+        status row_status DEFAULT 'active',
+        payment_status invoice_status DEFAULT 'unpaid',
+        payments JSONB,
+        created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+
+      );
+    `
+
+    const pool = this.pgConfig.getPool();
+    await pool.query(query);
+
+    this.logger.info(`Successfully created invoices table`);
+    return "invoices";
+  }
+
+
+}
